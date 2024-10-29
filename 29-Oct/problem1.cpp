5698b4a66beffabdaf86a4ea5562f41fdef83105
Muhammad Raffey <70153209@student.uol.edu.pk>
2024-10-29 07:21:08 +0000

    done basic

---
+++ 29-Oct/problem1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int age1,age2;
+	cout<<"enter the age: ";
+	cin>>age1;
+	cout<<"enter the friend age: ";
+	cin>>age2;
+	cout<< age1+age2<<endl;
+}
---
+++ 29-Oct/sumofTwoNums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	// BASIC INPUT OUTPUT
+	//	int a;
+	//	cin>>a;
+	//	cout<<"Hello I am Raffey.I am "<<a<<" Years Old."<<endl;
+
+	int a, b;
+	cout << "Enter TWo numbers :";
+	cin >> a;
+	cin >> b;
+	cout << "a+b= " << a + b;
+}
